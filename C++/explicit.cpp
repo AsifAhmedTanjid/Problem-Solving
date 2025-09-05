e0e62dc681237af4c91cc34c9df036331f707518
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-05 07:37:02 +0600

    implicit and explicit typecasting

---
+++ C++/explicit.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float PI=3.14;
+    cout <<(int)('A')<<endl;
+    cout<<(int)(PI)<<endl; //DATA LOSS
+    cout<<((float)10/3)<<endl;
+    cout<<(char)('A'+1);
+    return 0;
+}
---
+++ C++/implicit.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<10/3<<endl;
+    cout<<10/3.33<<endl;
+    cout<<'a'+1<<endl;
+    cout<<'A'-0;
+    return 0;
+}
