e1a2a60b4077f45a1c27258b878a54ab5eed2fa6
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-05 07:29:47 +0600

    const

---
+++ C++/const.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    const float PI = 3.14; //if const must be initialized
+    const float g = 9.8;
+    cout << PI << " " << g << endl;
+
+    return 0;
+}
