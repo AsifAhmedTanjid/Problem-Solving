bb1a9a54b15aedaa51a969d8283eeaa55769f4c3
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-06 13:25:47 +0600

    inverted triangle pattern

---
+++ C++/inverted-triangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the Number : ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n-i+1; j >0 ; j--)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
