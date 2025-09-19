bae213a1c100ccbdd11ccc2182ab3398226f8ef3
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-19 19:53:57 +0600

    parameterized-function

---
+++ D7/parameterized-function.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){ //a and b are parameters
+    int sum= a+b;
+    return sum;
+
+}
+int main(){
+
+    int result= sum(5,6); //5,6 are arguments
+    cout<<result;
+    return 0;
+}
