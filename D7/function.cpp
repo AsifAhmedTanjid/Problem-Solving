a99e0a6043a57b8ca3fb2858c55219530ac34edf
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-19 19:45:42 +0600

    function intro

---
+++ D7/function.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sayHello(){
+    cout<<"hello \n";
+}
+
+void assistant(){
+    sayHello();
+    cout<<"work done";
+}
+
+
+int main(){
+    // sayHello();
+    // sayHello();
+    // sayHello();
+    assistant();
+    return 0;
+}
