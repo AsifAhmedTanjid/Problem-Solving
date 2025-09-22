77b340b83ce6defc3999dca5db4d2d0552717fd5
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-22 22:52:05 +0600

    getter and setter methods

---
+++ OOPs/tempCodeRunnerFile.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+    string name;
+    float CGPA;
+
+public:
+
+    void getPercentage(){
+        cout<<(CGPA*25)<<"%"<<endl;
+
+    }
+
+    void setName(string nameVal){
+        name=nameVal;
+    }
+    void setCGPA(float CGPAVal){
+        CGPA=CGPAVal;
+    }
+
+    string getName(){
+        return name;
+    }
+    float getCGPA(){
+        return CGPA;
+    }
+
+
+};
+
+int main(){
+
+    
+    Student s1;
+    s1.setName("Asif");
+    s1.setCGPA(4.98);
+    cout<<s1.getName()<<" "<<s1.getCGPA()<<endl;
+
+    
+    return 0;
+}
